7f202bcd5035a7bc55a03e4ec804a70a22b774ff
shawon122334 <shawon.mu11@gmail.com>
2021-08-04 04:50:35 +0600

    initializing typescript project

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+var character = 'mario';
+console.log(character);
+//getting all three inputs
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
